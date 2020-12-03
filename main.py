7cec6cc4dfec41091282ec5defa27a893568043e
crepusculumx <crepusculumx@qq.com>
2020-12-03 19:43:45 +0800

    update

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import solver
 from controller import Controller
 
 if __name__ == '__main__':
-    # cube_s = "wbrryoyyowgowbbborgyrggw"
-    # cube = Cube(cube_s)
     controller = Controller()
-    # print(solve(cube, controller))
     solver.pre_solve(controller)
